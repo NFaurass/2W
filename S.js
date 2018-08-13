ea63ae01455e6ee77ab0cc859fb2464e4549cddc
NFaurass <36932043+NFaurass@users.noreply.github.com>
2018-08-13 16:47:35 +0000

    Update S.js

--- S.js
+++ S.js
@@ -1,8 +1,8 @@
 function en() {
-    alert(&quot;This Match Isn&#39;t Live Now !&quot;)
+    alert(&quot;This Match Isn&#39;t Live Now !&quot;);
 }
 function ar() {
-    alert(&quot;المباراة لا تجرى حاليا !&quot;)
+    alert(&quot;المباراة لا تجرى حاليا !&quot;);
 }
 function b1() {
     document.getElementById("SoccerLive").src = "https://Www.2wsoccer.tk";
@@ -11,14 +11,14 @@ function b2() {
    	document.getElementById("SoccerLive").src = "https://widestream.io/embed-39780";
 }
 function b3() {
-   	document.getElementById("SoccerLive").src = ""
+   	document.getElementById("SoccerLive").src = "";
 }
 function b4() {
-   	document.getElementById("SoccerLive").src = ""
+   	document.getElementById("SoccerLive").src = "";
 }
 function b5() {
-   	document.getElementById("SoccerLive").src = ""
+   	document.getElementById("SoccerLive").src = "";
 }
 function b6() {
-   	document.getElementById("SoccerLive").src = ""
+   	document.getElementById("SoccerLive").src = "";
 }
