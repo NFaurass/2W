23878079a0673a7c3ceaa908ec882d38a07bfaa3
NFaurass <36932043+NFaurass@users.noreply.github.com>
2019-11-07 20:54:08 +0000

    Create 07.js

---
+++ 2019/11/07.js
@@ -0,0 +1,29 @@
+const MatchData = [
+{
+  "FTeam": "بوروسيا مونشنغلادباخ",
+  "FTeamP": "https://i.postimg.cc/kGJSSn26/MGladbach.png",
+  "STeam": "روما",
+  "STeamP": "https://i.postimg.cc/59BpF0R0/Roma.png",
+  "League": "الدوري الاوروبي",
+  "Time": "20:00 GMT",
+  "Link": "http://www.beinleague.club/p/bein4tv.html"
+},
+{
+  "FTeam": "مانشستر يونايتد",
+  "FTeamP": "https://i.postimg.cc/Gtw7nTS0/Man_Utd.png",
+  "STeam": "بارتيزان بلجراد",
+  "STeamP": "https://ssl.gstatic.com/onebox/media/sports/logos/v_bEGhgMcIRbc7HdzgY1sw_96x96.png",
+  "League": "الدوري الاوروبي",
+  "Time": "20:00 GMT",
+  "Link": "http://www.beinleague.club/p/bein1tv.html"
+},
+{
+  "FTeam": "جلاسكو رينجرز",
+  "FTeamP": "https://ssl.gstatic.com/onebox/media/sports/logos/VJtO19cqg-zp3wuDuOwqAg_96x96.png",
+  "STeam": "بورتو",
+  "STeamP": "https://i.postimg.cc/zfm3YJFm/Porto.png",
+  "League": "الدوري الاوروبي",
+  "Time": "20:00 GMT",
+  "Link": "http://www.beinleague.club/p/bein2tv.html"
+}
+];
