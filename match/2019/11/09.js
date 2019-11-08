4b920b9238e75b981f64d9f3c8e7235be0fc76c6
NFaurass <36932043+NFaurass@users.noreply.github.com>
2019-11-08 19:44:40 +0000

    Create 09.js

---
+++ match/2019/11/09.js
@@ -0,0 +1,48 @@
+document.write( '<div id=\'app\'/>' );
+const MatchData = [
+       {
+  "FTeam": "ليستر سيتي",
+  "FTeamP": "https://i.postimg.cc/mrj6MrDC/Leicester.png",
+  "STeam": "أرسنال",
+  "STeamP": "https://i.postimg.cc/jjbGP9h0/Arsenal.png",
+  "League": "الدوري الانجليزي",
+  "Time": "18:30 GMT",
+  "Link": "http://www.beinleague.club/p/bein2tv.html"
+},
+{
+  "FTeam": "ريال مدريد",
+  "FTeamP": "https://i.postimg.cc/vTKmmMfd/Real_Madrid.png",
+  "STeam": "إيبار",
+  "STeamP": "https://i.postimg.cc/PqMrw1Yy/SDEibar.png",
+  "League": "الدوري الاسباني",
+  "Time": "18:30 GMT",
+  "Link": "http://www.beinleague.club/p/bein3tv.html"
+},
+{
+  "FTeam": "بايرن ميونيخ",
+  "FTeamP": "https://i.postimg.cc/s2W5dHFX/FCBayern.png",
+  "STeam": "بوروسيا دورتمود",
+  "STeamP": "https://i.postimg.cc/BnVCG1Xh/BVBDortmund.png",
+  "League": "الدوري الالماني",
+  "Time": "18:30 GMT",
+  "Link": "http://www.beinleague.club/p/bein5tv.html"
+},
+{
+  "FTeam": "نابولي",
+  "FTeamP": "https://i.postimg.cc/fR28Trgf/Napoli.png",
+  "STeam": "جنوى",
+  "STeamP": "https://i.postimg.cc/XYVPvFN4/Genoa.png",
+  "League": "الدوري الايطالي",
+  "Time": "20:45 GMT",
+  "Link": "http://www.beinleague.club/p/bein4tv.html"
+},
+{
+  "FTeam": "برشلونة",
+  "FTeamP": "https://i.postimg.cc/RCg68x3d/Barcelona.png",
+  "STeam": "سلتا فيغو",
+  "STeamP": "https://i.postimg.cc/Hnvr7bGR/Celta_Vigo.png",
+  "League": "الدوري الاسباني",
+  "Time": "21:00 GMT",
+  "Link": "http://www.beinleague.club/p/bein3tv.html"
+}
+];
